6dbbb98f48167a2bd999d93ebaf3bfc022472391
xafero <xafero@users.noreply.github.com>
2024-01-31 15:58:20 +0100

    Adding screenshot of current control helper

---
+++ src/specifics/Marrow.XPlat.Avalonia/Tools/PixelHelper.cs
@@ -0,0 +1,25 @@
+﻿using System.IO;
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Media.Imaging;
+
+namespace Marrow.XPlat.Avalonia.Tools
+{
+    public static class PixelHelper
+    {
+        public static MemoryStream RenderToImage(this Control control, int dpi = 96)
+        {
+            var pxSize = new PixelSize((int)control.Width, (int)control.Height);
+            var size = new Size(control.Width, control.Height);
+            var dpiV = new Vector(dpi, dpi);
+            using var bitmap = new RenderTargetBitmap(pxSize, dpiV);
+            control.Measure(size);
+            control.Arrange(new Rect(size));
+            bitmap.Render(control);
+            var mem = new MemoryStream();
+            bitmap.Save(mem);
+            mem.Position = 0L;
+            return mem;
+        }
+    }
+}
